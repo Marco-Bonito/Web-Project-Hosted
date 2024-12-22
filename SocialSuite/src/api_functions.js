042b6a5c10c4ec3addd0279a6bb1ef6f5678204f
Bonny9901 <marco.bonito.dev@gmail.com>
2024-12-22 20:56:05 +0100

    Update api_functions.js

--- SocialSuite/src/api_functions.js
+++ SocialSuite/src/api_functions.js
@@ -5,38 +5,39 @@ export default class ApiFunctions {
     }
 
     async register_user(userData = {}){
-
+        return userData
     }
 
     async get_user_data(userData = {}){
-
+        return userData
     }
 
     async create_user(userData = {}){
-
+        return userData
     }
 
     async update_user(userData = {}){
-
+        return userData
     }
 
     async delete_user(userData = {}){
-
+        return userData
     }
 
     async create_document(data = {}){
-
+        return data
     }
 
     async update_document(data = {}){
-
+        return data
     }
 
     async get_document(data = {}){
-
+        return data
     }
 
     async delete_document(data = {}){
-        
+        return data
     }
+    
 }
