0f34b009ba1b7624d98b8b452b0d09893388c880
Bonny9901 <marco.bonito.dev@gmail.com>
2024-12-23 19:03:27 +0100

    Update api_functions.js

--- SocialSuite/src/api_functions.js
+++ SocialSuite/src/api_functions.js
@@ -1,43 +1,65 @@
 export default class ApiFunctions {
 
     async login_user(userData = {}){
+        this.validate_data(userData , ['email' , 'password']);
         return userData
     }
 
     async register_user(userData = {}){
+        this.validate_data(userData , ['email' , 'password']);
         return userData
     }
 
     async get_user_data(userData = {}){
+        this.validate_data(userData , [] , ['ID' , 'email']);
         return userData
     }
 
     async create_user(userData = {}){
+        this.validate_data(userData , ['email']);
         return userData
     }
 
     async update_user(userData = {}){
+        this.validate_data(userData , [] ,['ID' , 'email']);
         return userData
     }
 
     async delete_user(userData = {}){
+        this.validate_data(userData , [] , ['ID' , 'email']);
         return userData
     }
 
     async create_document(data = {}){
+        this.validate_data(data , ['mainCollectionName'] , ['mainDocID' , 'subCollectionName' , 'subCollectionName']);
         return data
     }
 
     async update_document(data = {}){
+        this.validate_data(data , ['mainCollectionName'] , ['mainDocID' , 'subCollectionName' , 'subCollectionName']);
         return data
     }
 
     async get_document(data = {}){
+        this.validate_data(data , ['mainCollectionName'] , ['mainDocID' , 'subCollectionName' , 'subCollectionName']);
         return data
     }
 
     async delete_document(data = {}){
+        this.validate_data(data , ['mainCollectionName'] , ['mainDocID' , 'subCollectionName' , 'subCollectionName']);
         return data
     }
     
+    validate_data(data , necessaryField = [] , optionalField = []){
+        for (const key of data){
+            console.log(key)
+        }
+        necessaryField.forEach((field)=>{
+            console.log(field)
+        })
+
+        optionalField.forEach((optField)=>{
+            console.log(optField)
+        })
+    }
 }
